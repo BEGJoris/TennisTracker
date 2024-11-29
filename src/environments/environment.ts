3f4db717603c953c57efd9d2439c37d312c55784
BEGEL <begeljoris@gmail.com>
2024-11-29 22:28:22 +0100

    Init Firebase in the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCE9qEzuvHsGUxB8T3bbdMmzzhBLWlNjvw",
+    authDomain: "prog-multi.firebaseapp.com",
+    projectId: "prog-multi",
+    storageBucket: "prog-multi.firebasestorage.app",
+    messagingSenderId: "1089993476794",
+    appId: "1:1089993476794:web:1b7ad18c12f403c5ad5083"
+  }
 };
 
 /*
