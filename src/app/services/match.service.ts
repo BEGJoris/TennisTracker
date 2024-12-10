1ea8c2da19e0ff4a0d2d686ccc3d10a468dbab7f
BEGEL <begeljoris@gmail.com>
2024-12-10 16:31:11 +0100

    Refactor : Use BehaviorSubject for matches in the service + Add a BehaviorSubject for loading data

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -1,35 +1,73 @@
 import { Injectable } from '@angular/core';
 import {Match} from "../models/match.model";
 import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
-import {map, Observable} from "rxjs";
+import {BehaviorSubject, delay, first, map, Observable, take, takeLast, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchService {
-  // matchsRef:AngularFirestoreCollection<Match>;
   private dbPath = '/matchs';
   matchsRef:AngularFirestoreCollection<Match>;
+
   constructor(private _firestore: AngularFirestore) {
+
     this.matchsRef = this._firestore.collection<Match>(this.dbPath);
   }
 
-  getMatchsFromFirebase(): Observable<Match[]> {
+
+  private _loading$=new BehaviorSubject<boolean>(false);
+  get loading$(): Observable<boolean> {
+    return this._loading$.asObservable();
+  }
+
+  private _matchs$=new BehaviorSubject<Match[]>([]);
+  get matchs$(): Observable<Match[]> {
+    return this._matchs$.asObservable();
+  }
+
+
+  private setLoadingStatus(loading: boolean) {
+    this._loading$.next(loading);
+  }
+
+  getRecentMatches(){
+    this.setLoadingStatus(true);
+    this.matchsRef.snapshotChanges().pipe(
+      first(),
+      map((changes: any) => {
+        return changes.map((doc: any) => {
+          return ({id: doc.payload.doc.id, ...doc.payload.doc.data()}) as Match
+        });
+      }),
+      delay(2000),
+      map((matchs: Match[]) => matchs.slice(0, 3)),
+      tap((matchs: Match[]) =>{
+        this._matchs$.next(matchs)
+        this.setLoadingStatus(false);
+      }),
+    ).subscribe()
+  }
+
+  getMatchsFromFirebase() {
+    this.setLoadingStatus(true);
     return this.matchsRef.snapshotChanges().pipe(
       map((changes: any) => {
         return changes.map((doc: any) => {
           return ({id: doc.payload.doc.id, ...doc.payload.doc.data()}) as Match
         });
+      }),
+      tap((matchs: Match[]) => {
+        this._matchs$.next(matchs);
+        this.setLoadingStatus(false);
       })
-    )
+    ).subscribe()
   }
 
   getMatchById(id: string): Observable<Match> {
-    return new Observable(obs => {
-      this.matchsRef.doc(id).get().subscribe(doc => {
-        obs.next({id: doc.id, ...doc.data()} as Match);
-      })
-    })
+    return this.matchs$.pipe(
+      map((matchs: Match[]) => matchs.filter(match=>match.id===id)[0]),
+    )
   }
 
   createMatch(match: Match): Observable<Match> {
@@ -42,12 +80,19 @@ export class MatchService {
 
   updateMatch(match: Match): Observable<Match> {
     return new Observable(obs => {
-      this.matchsRef.doc(match.id).update(match)
+      this.matchsRef.doc(match.id.toString()).update(match)
       obs.next()
     })
   }
 
   deleteMatch(id: string): void {
+    this.setLoadingStatus(true);
     this._firestore.doc(`${this.dbPath}/${id}`).delete();
+    this.matchs$.pipe(
+      map((matchs: Match[]) => matchs.filter(match => match.id !== id)),
+      tap((matchs: Match[]) => this._matchs$.next(matchs))
+    )
+
+    this.setLoadingStatus(false);
   }
 }
