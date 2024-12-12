17c799161fc18259685032e218d116117723c70a
BEGEL <begeljoris@gmail.com>
2024-12-12 23:23:05 +0100

    feat : Add some fake datas for the global statistics page

--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {Match} from "../models/match.model";
+import {MatchService} from "../services/match.service";
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.page.scss'],
 })
 export class StatisticsPage implements OnInit {
-
-  constructor() { }
+  totalMatchs = 15;
+  victoirePourcentage = 66.7;
+  totalAces = 45;
+  totalDoubleFautes = 12;
+  pourcentagePremierService = 73;
+  pointsGagnes = 68;
+  derniersMatchs!:Observable<Match[]>
+  constructor(private _matchService: MatchService) { }
 
   ngOnInit() {
+    this._matchService.getRecentMatches()
+    this.derniersMatchs = this._matchService.matchs$
   }
 
 }
