c746a1f7442b38f6de21236d086e1a38e89cd046
BEGEL <begeljoris@gmail.com>
2024-12-22 18:26:22 +0100

    Export de l'application sous Android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCE9qEzuvHsGUxB8T3bbdMmzzhBLWlNjvw",
+    authDomain: "prog-multi.firebaseapp.com",
+    projectId: "prog-multi",
+    storageBucket: "prog-multi.firebasestorage.app",
+    messagingSenderId: "1089993476794",
+    appId: "1:1089993476794:web:1b7ad18c12f403c5ad5083"
+  }
 };
